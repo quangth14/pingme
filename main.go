2f6d9a6b956c4d320464915a7363b7e458b39ca0
quangth <quangth@cmccybersecurity.com>
2025-12-29 10:29:50 +0700

    add `ping` router

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Println("Hello, World!")
+
+	r := gin.Default()
+	r.POST("/ping", func(c *gin.Context) {
+		// print header
+		headersJSON, _ := json.MarshalIndent(c.Request.Header, "", "  ")
+		log.Printf("Request Headers:\n%s\n", string(headersJSON))
+
+		// print request body json pretty
+		var body map[string]interface{}
+		if err := c.ShouldBindJSON(&body); err != nil {
+			log.Printf("Request Body Error: %s\n", err.Error())
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		bodyJSON, _ := json.MarshalIndent(body, "", "  ")
+		log.Printf("Request Body:\n%s\n", string(bodyJSON))
+
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.Run(":8080")
 }
